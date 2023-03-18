5f5dfba3116faeabfc83d8c54d1563ff842a9964
Tender <tender121995@gmail.com>
2023-03-18 16:40:25 +0200

    fixed-filter-errors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ async function getPersons() {
 let card = document.querySelector(".card");
 let container = document.querySelector(".container");
 let cards = document.querySelectorAll(".card");
+let btnSearch = document.querySelector(".btnSearch");
+let inptSearch = document.querySelector(".searchInpt");
 
 //functie care imi creeaza carduri
 function createCard(person) {
@@ -98,36 +100,16 @@ btns.addEventListener("click", async (e) => {
 
 //filter by name
 
-function filterByName() {
-  // Get the search input value
-  const searchValue = document.querySelector(".search").value;
+btnSearch.addEventListener("click", async (e) => {
+  container = document.querySelector(".container");
 
-  // Get all the employee cards
-  const employeeCards = document.querySelectorAll(".card");
+  [].slice.call(container.children).forEach((e) => {
+    let name = e.querySelector(".name").textContent.toLowerCase();
 
-  // Create an array to hold the filtered cards
-  const filteredCards = [];
-
-  // Loop through each employee card
-  employeeCards.forEach((card) => {
-    // Get the employee name
-    const employeeName = card.querySelector(".name").textContent;
-
-    // If the employee name contains the search value, add the card to the filtered list
-    if (employeeName.includes(searchValue)) {
-      filteredCards.push(card);
+    if (!name.includes(inptSearch.value.toLowerCase())) {
+      e.classList.add("hide");
+    } else {
+      e.classList.remove("hide");
     }
   });
-
-  // If no cards match the search value, show all cards
-  if (filteredCards.length === 0) {
-    attachCards(employeeCards);
-  } else {
-    // Display the filtered list of employee cards
-    attachCards(filteredCards);
-  }
-}
-
-let btnSearch = document.querySelector(".btnSearch");
-
-btnSearch.addEventListener("click", filterByName);
+});
